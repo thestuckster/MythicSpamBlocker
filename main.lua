44d5dfba3d326a30a28ee7865c48e48b4a1aaa2d
thestuckster <sstucky3@gmail.com>
2019-06-25 00:23:12 -0500

    actually filtering properly. need to clean up code to be hardcoded if statements for every sub string

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 
 local function filterChat(self, event, text, playerName, languageName, channelName, playerName2, sepcialFlags, zoneChannelId, channelIndex, channelBaseName, _, lineId, guid, bnSenderId, isMoble) 
-    message('filter loaded')
-    
+    local filterFuncList = ChatFrame_GetMessageEventFilters(event)
+
     if(channelName == "2. Trade - City") then
         local powerLevel = 'power leveling'
         local powerLevel2 = 'powerleveling'
@@ -11,99 +11,52 @@ local function filterChat(self, event, text, playerName, languageName, channelNa
         local carry = 'carry'
         local mythicNumbers = '10/15/20'
         local goldOnly = 'gold only'
+        local boosting = 'boosting'
 
-        local lowerText = string.lower(text)
-        
-        if(lowerText:find(wts) ~= nil) then
-            message('someone is selling')
+        -- local keywords = {'power leveling', 'powerleveling', 'wts', 'm%+', 'mythic', 'carry', '10/15/20' 'gold only', 'boosting'}
 
-            if(lowerText:find(powerLevel) ~= nil) then 
-                return true
+        if(text ~= nil) then
+            local lowerText = string.lower(text)
+            if(lowerText:find(wts) ~= nil or lowerText:find('selling')) then
+                
+                if(lowerText:find(powerLevel) ~= nil) then 
+                    print('power level')
+                    return true
+                end
+    
+                if(lowerText:find(powerLevel2) ~= nil) then 
+                    print('power level2')
+                    return true
+                end
+    
+                if(lowerText:find(m) ~= nil) then
+                    print('m')
+                    return true
+                end
+    
+                if(lowerText:find(mythic) ~= nil) then
+                    print('mythic')
+                    return true
+                end
+    
+                if(lowerText:find(carry) ~= nil) then
+                    print('carry')
+                    return true
+                end
+    
+                if(lowertText:find(mythicNumbers) ~= nil) then
+                    print('numbers')
+                    return true
+                end
+    
+                if(lowerText:find(goldOnly) ~= nil) then
+                    print('gold only')
+                    return true
+                end
             end
-
-            if(lowerText:find(powerLevel2) ~= nil) then 
-                return true
-            end
-
-            if(lowerText:find(m) ~= nil) then
-                return true
-            end
-
-            if(lowerText:find(mythic) ~= nil) then
-                return true
-            end
-
-            if(lowerText:find(carry) ~= nil) then
-                return true
-            end
-
-            if(lowertText:find(mythicNumbers) ~= nil) then
-                return true
-            end
-
-            if(lowerText:find(goldOnly) ~= nil) then
-                return true
-            end
-            
-            return false, gsub(text)
         end
 
-        return false, gsub(text)
-    end
-
-    return false
+    end 
 end
 
 ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filterChat)
-
-
-
--- local chatFrame = CreateFrame("Frame")
--- chatFrame:RegisterEvent("CHAT_MSG_CHANNEL")
--- chatFrame:SetScript("OnEvent", function(self, event, text, playerName, languageName, channelName, playerName2, specialFlags, zoneChannelId, channelIndex, channelBaseName, _, lineId, guid, bnSenderId, isMobile)
-
---     if(channelName == "2. Trade - City") then
---         local powerLevel = 'power leveling'
---         local powerLevel2 = 'powerleveling'
---         local wts = 'wts'
---         local m = 'm%+'
---         local mythic = 'mythic'
---         local carry = 'carry'
---         local mythicNumbers = '10/15/20'
---         local goldOnly = 'gold only'
-
---         local lowerText = string.lower(text)
-        
---         if(lowerText:find(wts) ~= nil) then
---             -- message('someone is selling')
-
---             if(lowerText:find(powerLevel) ~= nil) then 
---                 message('powerLevel')
---             end
-
---             if(lowerText:find(powerLevel2) ~= nil) then 
---                 message('power level 2')
---             end
-
---             if(lowerText:find(m) ~= nil) then
---                 message('mythics')
---             end
-
---             if(lowerText:find(mythic) ~= nil) then
---             end
-
---             if(lowerText:find(carry) ~= nil) then
---                 message('carries')
---             end
-
---             if(lowertText:find(mythicNumbers) ~= nil) then
---                 message(mythicNumbers)
---             end
-
---             if(lowerText:find(goldOnly) ~= nil) then
---                 message(goldOnly)
---             end
-            
---         end
---     end
--- end)
