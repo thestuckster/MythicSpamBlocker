9b5749be0e340ba81d9db39f7c69f08a30f11ba6
stephen stucky <sstucky3@gmail.com>
2021-01-09 11:45:47 -0600

    update keywords, add slash command, update version #

--- main.lua
+++ main.lua
@@ -1,13 +1,21 @@
+local function MyAddonCommands(msg, editbox)
+    print("|cFF00FF00[MythicSpamBlocker]: |cFFFFFFFF version 1.1 https://www.curseforge.com/wow/addons/mythicspamblocker")    
+end
+  
+SLASH_HELLOWORLD1, SLASH_HELLOWORLD2 = '/msb', '/mythicspamblocker'
+  
+SlashCmdList["HELLOWORLD"] = MyAddonCommands   -- add /hiw and /hellow to command list
 
 local function filterChat(self, event, text, playerName, languageName, channelName, playerName2, sepcialFlags, zoneChannelId, channelIndex, channelBaseName, _, lineId, guid, bnSenderId, isMoble) 
     local filterFuncList = ChatFrame_GetMessageEventFilters(event)
     if(channelName == "2. Trade - City") then
         local wts = 'wts'
-        local keywords = {'gold only', 'power leveling', 'powerleveling', 'm%+', 'mythic', 'carry', '10/15/20', 'gold only', 'boosting', 'any key', 'guarantee time', 'pwrlvl'}
+        local keywords = {'gold only', 'power leveling', 'powerleveling', 'm%+', 'mythic', 'carry', '10/15/20', 'gold only%!', 'boosting', 'any key', 'guarantee time', 'pwrlvl',
+        'nathria', 'torghast', 'layer', 'carries', 'carry', 'keys', 'twisting', 'corridors', 'castle', 'castle nathria', 'aotc' }
         
         if(text ~= nil) then
             local lowerText = string.lower(text)
-            if(lowerText:find(wts) ~= nil or lowerText:find('selling')) then
+            if(lowerText:find(wts) ~= nil or lowerText:find('selling')) ~= nil then
                 for i, keyword in ipairs(keywords) do
                     if(lowerText:find(keyword) ~= nil) then
                         return true
